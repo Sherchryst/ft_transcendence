7f6ecaed2f2e444ab3240bf816ed28db8edaf7ee
Matthias Brunel <noreply@github.com>
2022-02-07 23:17:06 +0100

    add chat vue and freezeproof login

---
+++ front/src/views/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="chat">
+        <h1>Chat</h1>
+        <button v-on:click="go_to_home()">Home</button>
+    </div>
+</template>
+
+<script lang="ts">
+
+import router from '@/router';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    methods: {
+        go_to_home(): void {
+            router.push('/')
+        }
+    }
+})
+
+</script>
