bb22508c213c5613767c87d9336c688fc699037f
sgah <sgah@42.student.fr>
2022-02-14 15:12:54 +0100

    Fix: Forget to accept incoming change will merging main branch with vue-setup branch

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,12 +5,6 @@ import axios from 'axios'
 import './index.css'
 import store from './store'
 
-<<<<<<< HEAD
-createApp(App).use(router).mount('#app')
-axios.defaults.withCredentials = true
-localStorage.setItem("state", "0")
-=======
 
 createApp(App).use(store).use(router).mount('#app')
 axios.defaults.withCredentials = true
->>>>>>> acae2e22ba2a3a1bbaf979c0ba4102e7d2da2c8c
