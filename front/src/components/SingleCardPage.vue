9ed04de2a02f43415a38a70c7e8f2b2ce2d5541a
Romain Alvarez <roalvare@student.42.fr>
2022-02-14 14:46:22 +0100

    refactor: slot is clool

---
+++ front/src/components/SingleCardPage.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="grid grid-cols-12 place-content-center h-screen">
+		<div class="col-start-2 col-span-4 flex flex-col justify-between r-panel px-28 py-36">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+
+export default {
+}
+</script>
+
+<style lang="scss">
+.r-panel{
+	background: $bg--lg-color;
+	border-radius: 25px;
+}
+</style>
--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,30 +1,33 @@
 <template>
-	<div class="grid grid-cols-12 place-content-center h-screen">
-		<div class="col-start-2 col-span-4 flex flex-col justify-between r-panel px-28 py-36">
-			<div class="mb-20 text-left">
-				<h2 class="font-sans font-bold text-3xl pb-5">L'experience pong, autrement</h2>
-				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsum natus doloribus voluptas praesentium, ut quis nam sit, quam adipisci totam repellendus necessitatibus quas unde expedita. Asperiores impedit maxime labore?</p>
-			</div>
-			<ButtonLink text="Connection avec 42" route="/42" />
+	<single-card-page>
+		<div class="mb-20 text-left">
+			<main-title class="pb-5">L'experience pong, autrement</main-title>
+			<h2 class="font-sans font-bold text-3xl pb-5"></h2>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ipsum natus doloribus voluptas praesentium, ut quis nam sit, quam adipisci totam repellendus necessitatibus quas unde expedita. Asperiores impedit maxime labore?</p>
 		</div>
-	</div>
+		<ButtonLink text="Connection avec 42" route="/42" />
+	</single-card-page>
 </template>
 
 <script lang="ts">
 import ButtonLink from '@/components/ButtonLink.vue';
+import SingleCardPage from '@/components/SingleCardPage.vue';
+import MainTitle from '@/components/MainTitle.vue';
 
 export default {
 	components: {
-		ButtonLink
+		ButtonLink,
+		SingleCardPage,
+		MainTitle
 	}
 }
 </script>
 
 <style lang="scss">
-.r-panel{
-	background: $bg--lg-color;
-	border-radius: 25px;
-}
+// .r-panel{
+// 	background: $bg--lg-color;
+// 	border-radius: 25px;
+// }
 h2{
 	color: $text-title;
 }
