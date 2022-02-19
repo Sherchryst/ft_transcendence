c809765455c63b7962e2883f10b1ae362b722d75
Romain Alvarez <roalvare@student.42.fr>
2022-02-19 12:44:27 +0100

    feat: chat message

---
+++ front/src/components/DiscussionPreview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chat-preview flex flex-row items-center py-3 px-7 my-2">
+      <div class="mr-5">
+          <img class="h-16 w-16" src="/" alt="_profile">
+      </div>
+      <div class="text-left">
+          <p class="">Username</p>
+          <p class="font-thin text-xs">last message</p>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.chat-preview{
+    img {
+        background: $bg--lg-color;
+        border-radius: 50px;
+    }
+    > div > p {
+        color: $bg--lg-color;
+    }
+    &:hover{
+        background: $bg-action;
+    }
+    &.active{
+        background: $action;
+    }
+    border-radius: 16px;
+}
+</style>
--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="chat grid grid-cols-12 gap-5">
-      <div class="col-span-4 list">
-
+      <div class="col-span-4 list flex flex-col px-7 py-5">
+          <h4 class="text-left font-bold text-xl mb-4" >Messages directs</h4>
+          <discussion-preview class="active" />
+          <discussion-preview />
+          <discussion-preview />
+          <h4 class="text-left font-bold text-xl mt-4" >Channels</h4>
+          <discussion-preview />
       </div>
-      <div class="col-span-8 conversation">
-
+      <div class="col-span-8 conversation px-7 py-5">
+            <message side="1">
+                Lorem ipsum dolor
+            </message>
+            <message side="0">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt impedit mollitia numquam voluptatibus, necessitatibus amet neque sint hic soluta animi. Perspiciatis tenetur numquam harum eaque veritatis dicta odit illum magni.
+            </message>
+            <message side="1">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt impedit mollitia numquam voluptatibus, necessitatibus amet neque sint hic soluta animi. Perspiciatis tenetur numquam harum eaque veritatis dicta odit illum magni.
+            </message>
       </div>
   </div>
 </template>
 
 <script lang="ts">
+import DiscussionPreview from '@/components/DiscussionPreview.vue'
+import Message from '@/components/Message.vue'
 
 export default {
     components: {
+        DiscussionPreview,
+        Message
   },
 }
 </script>
+        Message
 
 <style lang="scss" scoped>
 .chat{
@@ -25,6 +43,9 @@ export default {
     }
     .list{
         background: $panel-color;
+        h4 {
+            color: $bg--lg-color;
+        }
     }
     .conversation{
         background: $panel--dk-color;
