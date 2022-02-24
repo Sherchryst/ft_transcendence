2288b8a2fa171d4d2db2c1c349aafe6b51bbd9d1
sgah <sgah@42.student.fr>
2022-02-24 03:00:49 +0100

    create:  Title panel components for profile

---
+++ front/src/components/TitlePanel.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="flex flex-col">
+		<div class="flex flew-rows jus">
+			<div class="icon">
+				<slot></slot>
+			</div>
+			<div class="title justify-self-start">
+				<h2 class="font-sans text-3xl">{{title}}</h2>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+	title: String
+  }
+})
+
+export default class ProfilePanel extends Vue {
+	title!: string;
+}
+</script>
+
+<style lang="scss">
+.icon{
+	width: 44px;
+	height: 44px;
+	border-radius: 25px;
+}
+.title{
+	width: auto;
+	height: 44px;
+	left: 720px;
+	top: 171px;
+}
+h3{
+	color: $text-title;
+}
+</style>
