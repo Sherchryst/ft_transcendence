ce71478574155298532fe2ddc2e742b382b9830c
sgah <sgah@42.student.fr>
2022-02-24 02:59:41 +0100

    create: creation of profile picture component for profile

---
+++ front/src/components/ProfilePicture.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="frame flex flex-shrink-0 w-auto h-auto ">
+		<img class="h-64 w-64" src="../assets/logo.png" alt="profile">
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.frame {
+  border: 10px solid ;
+  margin: auto;
+  padding: 15px 25px;
+  border-radius: 94px;
+}
+</style>
