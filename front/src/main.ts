4291d476a05ce0dba02f31741a6e6d6eb9e450bb
sgah <sgah@42.student.fr>
2022-03-15 14:36:59 +0100

    Add: add Ts synthax to vuex

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import './index.css'
-import store from './store'
+import { store, key } from './store'
 import { createMetaManager } from 'vue-meta'
 
 
 createApp(App)
-	.use(store)
+	.use(store, key)
 	.use(router)
 	.use(createMetaManager())
 	.mount('#app')
--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,9 +1,26 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue'
+import { createStore, Store } from 'vuex'
+import axios from 'axios';
 
-export default createStore({
+// define your typings for the store state
+export interface State {
+  profile: {user: unknown, friends: unknown[], achievements: unknown[]}
+}
+
+// define injection key
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
   state: {
+	profile: {user: 0, friends: [], achievements: []}
   },
   mutations: {
+	connect() {
+		axios.get('http://localhost:3000/users/get-profile').then((res) => {
+		this.profile = res.data;
+		console.log("hop", this.profile)
+	})
+	}
   },
   actions: {
   },
