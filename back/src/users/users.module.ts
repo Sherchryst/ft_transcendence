df4a27252f753fc2d316a802b51b1a6e8356e7b6
Matthias Brunel <noreply@github.com>
2022-03-18 00:46:29 +0100

    add auth check for user requests

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Achievement } from './entities/achievement.entity';
 import { Avatar } from './entities/avatar.entity';
@@ -6,6 +6,8 @@ import { User } from './entities/user.entity';
 import { UserAchievement } from './entities/user-achievement.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersGateway } from './users.gateways';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
@@ -13,9 +15,11 @@ import { UsersController } from './users.controller';
     Achievement,
     Avatar,
     User,
-    UserAchievement
-  ])],
-  providers: [UsersService],
-  exports: [UsersService]
+    UserAchievement,
+  ]),
+  forwardRef(() => AuthModule)
+],
+  providers: [UsersService, UsersGateway],
+  exports: [UsersService],
 })
 export class UsersModule {}
