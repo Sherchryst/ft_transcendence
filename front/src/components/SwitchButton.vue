79d86149c91a901751f754664e0a025c69382e0c
sgah <sgah@42.student.fr>
2022-03-31 17:18:21 +0200

    ADD: add defineComponent from vue to SwitchButton component

--- front/src/components/SwitchButton.vue
+++ front/src/components/SwitchButton.vue
@@ -6,7 +6,11 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+    name: "SwitchButton",
+})
 </script>
 
 <style lang="scss" scoped>
