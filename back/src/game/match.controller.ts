e2307ba6877557143d0c5b490c7c5d02dd791d95
Adrien Soursou <asoursou@student.42.fr>
2022-04-11 05:57:26 +0200

    Renamed some methods

--- back/src/game/match.controller.ts
+++ back/src/game/match.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { Jwt2faGuard } from 'src/auth/jwt/jwt.guard';
 import { MatchService } from './match.service';
 
 @Controller('match')
-//@UseGuards(Jwt2faGuard)
+@UseGuards(Jwt2faGuard)
 export class MatchController {
   constructor(private matchsService: MatchService) {}
 
   @Get('get-history')
-  async profile(@Query('userId') userId: number, @Query('limit') limit: number) {
+  async getHistory(@Query('userId') userId: number, @Query('limit') limit: number) {
     const history = await this.matchsService.getHistory(userId, limit);
     return JSON.stringify(history);
   }
 
   @Get('get-winrate')
-  async winrate(@Query('userId') userId: number) {
+  async getWinrate(@Query('userId') userId: number) {
     const winrate = await this.matchsService.getWinrate(userId);
     return { winrate: winrate };
   }
