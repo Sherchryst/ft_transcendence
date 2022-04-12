cd8e21b97b81c35320d7aaaa443fe0ecef6a85c4
Romain Alvarez <roalvare@student.42.fr>
2022-04-12 15:06:34 +0200

    feat: move notification

--- front/src/components/Notification/NotifGame.vue
+++ front/src/components/Notification/NotifGame.vue
@@ -1,10 +1,17 @@
 <template>
-	<span class="font-accentuate font-semibold">{{username}}</span> wants to challenge you on <span class="font-accentuate font-semibold">{{mapName}}</span>
+	<div>
+		<span class="font-accentuate font-semibold">{{username}}</span> wants to challenge you on <span class="font-accentuate font-semibold">{{mapName}}</span>
+		<div class="grid grid-cols-2 gap-3 my-2">
+			<ButtonLink @click="acceptInvitation">Accept</ButtonLink>
+			<ButtonLink @click="close" class="btn-neutral">Decline</ButtonLink>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { GameInvitation } from "@/interfaces/Notification";
+import ButtonLink from "../ButtonLink.vue";
 import { gameSocket } from "@/socket";
 
 export default defineComponent({
@@ -17,15 +24,21 @@ export default defineComponent({
 			socket: gameSocket 
 		}
 	},
+	components: { ButtonLink },
 	methods: {
 		acceptInvitation() {
 			this.socket.emit("acceptInvit", this.data);
 			console.log("acceptInvit", this.data);
+			this.close()
 		},
 		declineInvitation() {
-			this.socket.emit("declineInvit", this.data);
+			// this.socket.emit("declineInvit", this.data);
 			console.log("declineInvit", this.data);
+			this.close()
 		},
+		close(){
+			this.$emit("close", this.invitation)
+		}
 		// 		gameSocket.on("invited", (data : string) => {
 		// 	gameSocket.emit("acceptInvit", data); // TODO: ask for confirmation
 		// 	console.log("accepted invite : ", data);
@@ -35,13 +48,16 @@ export default defineComponent({
 		// })
 	},
 	computed: {
+		invitation(): GameInvitation {
+			return (this.data as GameInvitation)
+		},
 		username(): string {
-			return this.data.from.nickname;
+			return this.data.from.login;
 		},
 		mapName(): string {
 			// return this.data.map.name;
-			return ("")
-		}
-	}
+			return ("Centurion")
+		},
+	},
 })
 </script>
--- front/src/components/Notification/NotifPanel.vue
+++ front/src/components/Notification/NotifPanel.vue
@@ -22,14 +22,9 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import NotifCard from '@/components/Notification/NotifCard.vue';
-import { Notification, FriendRequest, GameStart } from "@/interfaces/Notification";
-// import { GameInvitation, ChannelInvitation } from "@/interfaces/Notification";
-// import { User } from "@/interfaces/Profile";
-import { API } from "@/scripts/auth";
 import NotifFriend from "./NotifFriend.vue";
 import NotifGame from "./NotifGame.vue";
 import NotifChannel from "./NotifChannel.vue";
-import { gameSocket } from "@/socket";
 
 export default defineComponent({
 	name: 'NotifPanel',
@@ -39,87 +34,13 @@ export default defineComponent({
 		NotifGame,
 		NotifChannel,
 	},
-	data() {
-		return {
-			socket: gameSocket,
-			notifications: [] as Notification[]
-		}
+	props: {
+		notifications: Array
 	},
 	methods: {
-		getNotif(): void {
-			API.get("/users/get-friend-requests", {
-				params: {
-					id: this.$store.getters.getId
-				}
-			}).then( (response) => {
-				response.data.forEach( (element: FriendRequest) => {
-					this.addFriendRequest(element)
-				});
-			})
-
-			// TEST
-			
-			// let user = {
-			// 	id: 4,
-			// 	login: "lnoirot",
-			// 	mmr: 0,
-			// 	nickname: "Tor",
-			// 	role: "user",
-			// 	newUser: false
-			// } as User
-			// let channel = {
-			// 	id: 3,
-			// 	name: "beginner",
-			// 	visibility: 1,
-			// 	password: "",
-			// }
-			// let dateEvent = new Date()
-			// let gameRequest = { from: user, map: "Andromeda"} as GameInvitation
-			// let ChannelRequest = { from: user, channel: channel} as ChannelInvitation
-			// this.notifications.push({
-			// 	container: 'NotifChannel',
-			// 	content: ChannelRequest,
-			// 	date: dateEvent
-			// } as Notification)
-			// this.notifications.push({
-			// 	container: 'NotifGame',
-			// 	content: gameRequest,
-			// 	date: dateEvent
-			// } as Notification)
-
-		},
-		addFriendRequest(data: FriendRequest): void {
-			let friendRequest =  { nickname: data.nickname, id: data.id} as FriendRequest
-			let dateEvent = new Date()
-			this.notifications.push({
-				container: 'NotifFriend',
-				content: friendRequest,
-				date: dateEvent
-			} as Notification)
-		},
-		addGameStart(data: string): void {
-			let gameStart = JSON.parse(data) as GameStart
-			let dateEvent = new Date()
-			this.notifications.push({
-				container: 'NotifGameStart',
-				content: gameStart,
-				date: dateEvent
-			} as Notification)
-		},
 		removeNotif(data: any): void {
-			let index = this.notifications.findIndex( (notif) => {
-				return (notif.content == data)
-			})
-			console.log("index", index)
-			this.notifications.splice(index)
-		}
+			this.$emit('close', data)
+		},
 	},
-	created() {
-		console.log("NotifPanel created")
-	},
-	mounted() {
-		console.log("NotifPanel mounted")
-		this.getNotif()
-	}
 })
 </script>
