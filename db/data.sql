ac1029df92f53c1201f64d86358d6977f8a7ee6f
Adrien Soursou <asoursou@student.42.fr>
2022-04-12 03:30:32 +0200

    Wait, you don't like raw food?

--- db/data.sql
+++ db/data.sql
@@ -23,3 +23,15 @@ INSERT INTO "game_map" ("ball_color", "background_color", "stars_color", "racket
 
 INSERT INTO "game_map" ("ball_color", "background_color", "stars_color", "racket_color") VALUES
 (CAST(x'b00b69' AS int), CAST(x'0e1125' AS int), CAST(x'b48700' AS int), CAST(x'833260' AS int));
+
+INSERT INTO "achievement" ("name", "description") VALUES
+('You are LOSER', 'Lose 1 match'),
+('You are a WINNER', 'Win 5 matches'),
+('Perfect', 'Win a match taking no casualties'),
+('Traveler', 'Make a match on each of the maps');
+
+INSERT INTO "user_achievement" ("user_id", "achievement_id") VALUES
+(1, 1), (1, 3);
+
+INSERT INTO "match" ("mode", "begin_at", "score1", "score2", "map_id", "player1_id", "player2_id", "winner_id") VALUES
+('ranked', CURRENT_TIMESTAMP, 11, 0, 1, 1, 2, 1);
