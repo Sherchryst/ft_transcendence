b213426d6b4395409506eac9654c188a4c0324e1
sherchryst <sherchryst@icloud.com>
2022-04-13 14:56:40 +0200

    Fix: Ts clean error on array type

--- front/src/components/profile/Achievements.vue
+++ front/src/components/profile/Achievements.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid md:grid-cols-2 gap-x-10 gap-y-5">
-        <div v-for="(achiev, index) in AchievementsTab" :key="index">
+        <div v-for="(achiev, index) in achievements.slice(0, 4)" :key="index">
             <LargerCard v-if="achiev.unlocked_at != null" :description="achiev.description.toUpperCase()" :title="achiev.name.toUpperCase()"></LargerCard>
             <LargerCard v-else class="lg-card-inactive" :description="achiev.description.toUpperCase()" :title="achiev.name.toUpperCase()"></LargerCard>
         </div>
@@ -15,23 +15,10 @@ import { Achievement } from "@/interfaces/Profile";
 export default defineComponent({
     name: "Achievements",
     props: {
-        achievementsTab: { type: Array, required: true },
-    },
-    data() {
-        return {
-            AchievementsTab: [] as Achievement[],
-        };
+        achievements: { type:  Array as () => Achievement[], required: true },
     },
     components: {
         LargerCard
-    },
-    watch: {
-        achievementsTab: {
-            handler(newVal: Achievement[]) {
-                this.AchievementsTab = newVal.slice(0, 4);
-            },
-            deep: true,
-        },
     },
 });
 </script>
